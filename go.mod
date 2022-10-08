9039900028cda5f8f5a4450e0651c7995a21d434
Mohd Salman Ansari <salman0ansariii@gmail.com>
2022-10-08 19:18:47 +0530

    update main.go and go.mod

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module golly-bot
 go 1.19
 
 require (
-	github.com/bwmarrin/discordgo v0.26.1 // indirect
+	github.com/bwmarrin/discordgo v0.26.1 
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/joho/godotenv v1.4.0 
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
--- main.go
+++ main.go
@@ -1,31 +1,69 @@
 package main
 
 import (
-	// Import the Discordgo package, and other required packages.
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
+
+var Token string
 
 // Init function, called on startup, before the main function.
 func init() {
 	// Setup required variables and other objects here.
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file.")
+	}
 }
 
 // Main function of the bot, called on startup.
 func main() {
-	// Create a new Discord session using the provided bot token.
+	Token = os.Getenv("TOKEN")
+	bot, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating discod session", err)
+		return
+	}
 
-	// Register a function as callback for MessageCreate events.
+	bot.AddHandler(messageCreate)
+	// We care about receiving message events.
+	bot.Identify.Intents = discordgo.IntentsGuildMessages
 
 	// Open a websocket connection to Discord and begin listening.
+	err = bot.Open()
+	if err != nil {
+		fmt.Println("error opening connection", err)
+		return
+	}
 
 	// Wait until CTRL-C or other term signal is received.
+	fmt.Println("Bot is running. \nPress CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGTERM)
+	<-sc
 
 	// Close the websocket gracefully.
+	bot.Close()
 }
 
 // MessageCreate is called whenever a new message is created on any channel that the authenticated bot has access to.
 // Set parameters accordingly.
-func messageCreate() {
-	// Ignore all messages created by the bot itself
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	//Develop commands to the bot here.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
